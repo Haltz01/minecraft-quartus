dfb38132a79d3292c6947d80f3b588dbbc8728fe
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-06-17 10:38:11 -0300

    Add Multiplexer

---
+++ src/main/java/io/github/marcuscastelo/quartus/block/circuit_components/MultiplexerGateBlock.java
@@ -0,0 +1,25 @@
+package io.github.marcuscastelo.quartus.block.circuit_components;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiplexerGateBlock extends AbstractGateBlock{
+    @Override
+    public List<Direction> getPossibleInputDirections(World world, BlockPos pos) {
+        BlockState bs = world.getBlockState(pos);
+        Direction facingDirection = bs.get(FACING);
+        return Arrays.asList(facingDirection.rotateYClockwise(), facingDirection.rotateYCounterclockwise(), facingDirection.getOpposite());
+    }
+
+    @Override
+    public List<Direction> getPossibleOutputDirections(World world, BlockPos pos) {
+        BlockState bs = world.getBlockState(pos);
+        Direction facingDirection = bs.get(FACING);
+        return Arrays.asList(facingDirection);
+    }
+}
