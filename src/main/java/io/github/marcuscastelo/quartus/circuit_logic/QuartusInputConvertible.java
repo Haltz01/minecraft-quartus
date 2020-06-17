fe64f97ab0b810ff1e516c81a7bcbe766fad3bae
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-06-17 10:40:44 -0300

    Fix unmatch logic and throwing

--- src/main/java/io/github/marcuscastelo/quartus/circuit_logic/QuartusInputConvertible.java
+++ src/main/java/io/github/marcuscastelo/quartus/circuit_logic/QuartusInputConvertible.java
@@ -1,24 +1,27 @@
 package io.github.marcuscastelo.quartus.circuit_logic;
 
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public interface QuartusInputConvertible extends QuartusNodeConvertible {
-    QuartusInput createQuartusInput(World world, BlockPos pos);
+    QuartusInput createQuartusInput(World world, BlockPos pos) throws QuartusNode.QuartusWrongNodeBlockException;
     List<Direction> DIRECTIONS_NONE = new ArrayList<>();
 
     @Override
-    default QuartusNode createQuartusNode(World world, BlockPos pos) {
+    default QuartusNode createQuartusNode(World world, BlockPos pos) throws QuartusNode.QuartusWrongNodeBlockException {
         return createQuartusInput(world, pos);
     }
 
     @Override
     default List<Direction> getPossibleOutputDirections(World world, BlockPos pos) {
-        return CircuitUtils.getHorizontalDirections();
+        Direction facingDirection = world.getBlockState(pos).get(Properties.FACING);
+        return Arrays.asList(facingDirection.getOpposite());
     }
 
     @Override
--- src/main/java/io/github/marcuscastelo/quartus/circuit_logic/QuartusOutputConvertible.java
+++ src/main/java/io/github/marcuscastelo/quartus/circuit_logic/QuartusOutputConvertible.java
@@ -1,13 +1,31 @@
 package io.github.marcuscastelo.quartus.circuit_logic;
 
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public interface QuartusOutputConvertible extends QuartusNodeConvertible {
-    QuartusOutput createQuartusOutput(World world, BlockPos pos);
+    QuartusInput createQuartusInput(World world, BlockPos pos) throws QuartusNode.QuartusWrongNodeBlockException;
+    List<Direction> DIRECTIONS_NONE = new ArrayList<>();
 
     @Override
-    default QuartusNode createQuartusNode(World world, BlockPos pos) {
-        return createQuartusOutput(world, pos);
+    default QuartusNode createQuartusNode(World world, BlockPos pos) throws QuartusNode.QuartusWrongNodeBlockException {
+        return createQuartusInput(world, pos);
+    }
+
+    @Override
+    default List<Direction> getPossibleOutputDirections(World world, BlockPos pos) {
+        return DIRECTIONS_NONE;
+    }
+
+    @Override
+    default List<Direction> getPossibleInputDirections(World world, BlockPos pos) {
+        final Direction facingDirection = world.getBlockState(pos).get(Properties.FACING);
+        return Arrays.asList(facingDirection.getOpposite());
     }
 }
