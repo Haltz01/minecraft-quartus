e23340337c3283260c7dd89d543088b7b3a72bc0
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-06-17 10:37:26 -0300

    Remodel Input, Add output

--- src/main/java/io/github/marcuscastelo/quartus/block/circuit_components/OutputBlock.java
+++ src/main/java/io/github/marcuscastelo/quartus/block/circuit_components/OutputBlock.java
@@ -1,21 +1,20 @@
 package io.github.marcuscastelo.quartus.block.circuit_components;
 
 import io.github.marcuscastelo.quartus.circuit_logic.QuartusInput;
-import io.github.marcuscastelo.quartus.circuit_logic.QuartusInputConvertible;
-import io.github.marcuscastelo.quartus.circuit_logic.QuartusNode;
+import io.github.marcuscastelo.quartus.circuit_logic.QuartusOutputConvertible;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class InputBlock extends AbstractNodeBlock implements QuartusInputConvertible {
-    public InputBlock() {
+public class OutputBlock extends AbstractNodeBlock implements QuartusOutputConvertible {
+    public OutputBlock() {
         super(Settings.copy(Blocks.LEVER));
     }
 
     @Override
-    public QuartusInput createQuartusInput(World world, BlockPos pos) throws QuartusNode.QuartusWrongNodeBlockException {
+    public QuartusInput createQuartusInput(World world, BlockPos pos) {
         return new QuartusInput(world, pos) {
             @Override
             public boolean getInGameValue() {
