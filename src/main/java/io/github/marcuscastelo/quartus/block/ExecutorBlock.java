c83c1c44baca9d9d5d46e2a2ab4625df1a6d6efb
marcuscastelo <viniciuscastelo6@hotmail.com>
2020-06-17 10:28:26 -0300

    Add Executor

---
+++ src/main/java/io/github/marcuscastelo/quartus/block/ExecutorBlock.java
@@ -0,0 +1,26 @@
+package io.github.marcuscastelo.quartus.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.util.math.Direction;
+
+public class ExecutorBlock extends HorizontalFacingBlock {
+    public ExecutorBlock() {
+        super(Settings.copy(Blocks.ANVIL));
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+}
