af7da57bffc281a9f0b08134ab9364e0d2f020ce
daltonsato <daltonhiroshisato@hotmail.com>
2020-07-23 00:24:19 -0300

    Send help at line 87 to 101 Documentation required

--- src/main/java/io/github/marcuscastelo/quartus/circuit/components/QuartusCircuitComponent.java
+++ src/main/java/io/github/marcuscastelo/quartus/circuit/components/QuartusCircuitComponent.java
@@ -11,7 +11,14 @@ import sun.awt.X11.XSystemTrayPeer;
 import java.sql.Connection;
 import java.util.*;
 
+/**
+ * Classe que define genericamente um componente do circuito, utilizado no Mod
+ */
 public class QuartusCircuitComponent {
+	/**
+	 * Sub-Classe que define a direção que a informação que passa
+	 * por um componente deve seguir, de acordo com seus inputs e outputs
+	 */
     public static class QuartusCircuitComponentDirectionInfo {
         public final ImmutableList<Direction> possibleInputDirections, possibleOutputDirections;
         public QuartusCircuitComponentDirectionInfo(List<Direction> possibleInputDirections, List<Direction> possibleOutputDirections) {
@@ -47,6 +54,12 @@ public class QuartusCircuitComponent {
     private final int ID;
     private final String componentName;
 
+	/**
+	 * Contrutor padrão da classe QuartusCircuitComponent
+	 * @param componentName	->	Nome do componente
+	 * @param possibleDirectionsInfo	->	Possíveis direções que a informação pode seguir
+	 * @param ID	->	Identificador do componente
+	 */
     public QuartusCircuitComponent(String componentName, QuartusCircuitComponentDirectionInfo possibleDirectionsInfo, int ID) {
         this.componentName = componentName;
         this.connections = new HashMap<>();
@@ -71,6 +84,8 @@ public class QuartusCircuitComponent {
         }
     }
 
+	//NÃO SEI O QUE FAZEM!!!
+	//PLS, SEND HELP
     public QuartusCircuitComponent(String componentName, QuartusCircuitComponentDirectionInfo possibleDirectionsInfo) {
         this(componentName, possibleDirectionsInfo, LAST_ID++);
     }
@@ -85,6 +100,7 @@ public class QuartusCircuitComponent {
         this.logic = logic;
     }
 
+	//Método que retorna o ID de um componente
     public int getID() { return ID; }
 
     //TODO: tornar mais genérica: atualmente foca apenas em trazer a saída do outro (supondo ser única) para a entrada deste (supondo ser única)
@@ -115,13 +131,32 @@ public class QuartusCircuitComponent {
         }
     }
 
+	/**
+	 * Método que faz a chamada do updateInputInfo de um circuito,
+	 * atualizando seus valores de entrada e saída
+	 * @param circuit	->	Circuito a ser atualizado
+	 */
     public void updateComponent(QuartusCircuit circuit) {
         updateInputInfo(circuit);
         if (logic != null) logic.updateLogic(inputInfo, outputInfo);
     }
 
-    public Map<Direction, QuartusBusInfo> getOutputInfo() { return outputInfo; }
-    public Map<Direction, QuartusBusInfo> getInputInfo() { return inputInfo; }
+	/**
+	 * Método que retorna as informações que estão nos outputs de um componente
+	 * @return	->	Mapeamento dos Bus's com suas respectivas informações
+	 */
+	public Map<Direction, QuartusBusInfo> getOutputInfo() { return outputInfo; }
+	
+	/**
+	 * Método que retorna as informações que estão nos inputs de um componente
+	 * @return	->	Mapeamento dos Bus's com suas respectivas informações
+	 */
+	public Map<Direction, QuartusBusInfo> getInputInfo() { return inputInfo; }
+	
+	/**
+	 * Método que verifica se um dado componente possui conexões nas suas saídas
+	 * @return	->	Boolean que verifica se há conexões nas saídas
+	 */
     public boolean hasOutputConnections() {
         for (List<ComponentConnection> connectionsPerDir: connections.values()) {
             for (ComponentConnection connection: connectionsPerDir) {
@@ -131,10 +166,19 @@ public class QuartusCircuitComponent {
         return false;
     }
 
+	/**
+	 * Método que adiciona uma conexão a um componente de acordo com a direção dada
+	 * @param direction	->	Direção dada para adicionar conexão
+	 * @param connection	->	Conexão a ser adicionada
+	 */
     public void addConnection(Direction direction, ComponentConnection connection) {
         connections.get(direction).add(connection);
     }
 
+	/**
+	 * Método que retorna como String as conexões nos outputs de um componente
+	 * @return	->	String com as conexões das saídas do componente
+	 */
     public String getOutputConnectionsString() {
         StringBuilder str = new StringBuilder();
 
@@ -150,6 +194,10 @@ public class QuartusCircuitComponent {
         return str.toString();
     }
 
+	/**
+	 * Método que retorna uma lista com as conexões das saídas de um componente
+	 * @return	->	Lista com as conexões
+	 */
     public List<ComponentConnection> getOutputConnections() {
         List<ComponentConnection> outputConnections = new ArrayList<>();
         for (List<ComponentConnection> connectionsPerDir: connections.values())
@@ -159,6 +207,10 @@ public class QuartusCircuitComponent {
         return outputConnections;
     }
 
+	/**
+	 * Método que retorna uma lista com as conexões nos inputs de um componente
+	 * @return
+	 */
     public List<ComponentConnection> getInputConnections() {
         List<ComponentConnection> inputConnections = new ArrayList<>();
         for (List<ComponentConnection> connectionsPerDir: connections.values())
@@ -168,13 +220,24 @@ public class QuartusCircuitComponent {
         return inputConnections;
     }
 
+	/**
+	 * Método que retorna um mapeamento das conexões dos componentes
+	 * e suas direções
+	 * @return	->	Mapeamento das conexões e direções
+	 */
     public Map<Direction, List<ComponentConnection>> getConnections() {
         return connections;
     }
 
+	//Métodos que retornam uma lista com as possíveis direções de inputs e outputs, respectivamente
     public final ImmutableList<Direction> getPossibleInputDirections() { return possibleDirectionsInfo.possibleInputDirections; }
     public final ImmutableList<Direction> getPossibleOutputDirections() { return possibleDirectionsInfo.possibleOutputDirections; }
 
+	/**
+	 * Método que retorna a toString de um componente, adicionando seu ID
+	 * no final da String, facilitando identificação dos componentes
+	 * durante compilição e execução
+	 */
     @Override
     public String toString() {
         return componentName+"_"+getID();
